2cc8ecbed68ed2efd3e6de0da24256603a6eab6b
Egor Yakovlev <egorsambo0405@mail.ru>
2023-06-28 14:09:34 +0300

    Start project

--- static/js/script.js
+++
@@ -1,7 +0,0 @@
-function hel() {
-    var word = "0000"
-    var pass = document.querySelector("#pass").value;
-    if (word != pass) {
-        alert(['"' + pass + '" - неверный пароль'])
-    }
-}
