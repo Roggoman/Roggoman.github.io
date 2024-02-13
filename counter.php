b03473895e3f78f7b2357d79aa8f4d3e061f3bc0
Egor Yakovlev <egorsambo0405@mail.ru>
2024-02-13 20:31:36 +0300

    New Era

--- counter.php
+++ counter.php
@@ -1,10 +1,34 @@
 <?php
-  $counterFile = 'counter.txt';
-  if (!file_exists($counterFile)) {
-    file_put_contents($counterFile, '0');
+  // Имя куки для идентификации пользователя
+  $cookieName = 'site_visit';
+
+  // Проверяем, установлено ли уже куки
+  if (!isset($_COOKIE[$cookieName])) {
+    // Если куки не установлено, увеличиваем счетчик и устанавливаем куки
+    $counterFile = 'counter.txt';
+
+    // Проверяем существует ли файл счетчика
+    if (file_exists($counterFile)) {
+      // Если файл существует, читаем значение счетчика
+      $counter = (int)file_get_contents($counterFile);
+      // Увеличиваем значение счетчика на 1
+      $counter++;
+    } else {
+      // Если файл не существует, создаем новый счетчик и устанавливаем его значение в 1
+      $counter = 1;
+    }
+
+    // Записываем новое значение счетчика обратно в файл
+    file_put_contents($counterFile, $counter);
+
+    // Устанавливаем куки на 24 часа
+    setcookie($cookieName, 'visited', time() + (24 * 3600));
+  } else {
+    // Если куки уже установлено, просто считываем значение счетчика из файла
+    $counterFile = 'counter.txt';
+    $counter = (int)file_get_contents($counterFile);
   }
-  $counter = (int)file_get_contents($counterFile);
-  $counter++;
-  file_put_contents($counterFile, $counter);
+
+  // Выводим значение счетчика на страницу
   echo $counter;
 ?>
